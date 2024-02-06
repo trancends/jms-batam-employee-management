f8d16feaf3a33f3af8a8b5eb32a9b489d6c8b17d
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-02-06 12:56:27 +0700

    feat: scaffold detail-employee component

---
+++ src/app/detail-employee/detail-employee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-employee',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-employee.component.html',
+  styleUrl: './detail-employee.component.css'
+})
+export class DetailEmployeeComponent {
+
+}
