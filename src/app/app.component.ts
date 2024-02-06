b6f287e129d852a892da56026cb0234a83aeb334
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-02-06 12:50:08 +0700

    feat: add employee data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  // styleUrl: './app.component.css'
+  template: `<h1>Hello, {{ title }}</h1>`,
+  styles: ``,
 })
 export class AppComponent {
   title = 'employee-management';
